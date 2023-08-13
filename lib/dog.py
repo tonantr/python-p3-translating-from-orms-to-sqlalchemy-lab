9bb24866bca5091ee14865f7eea8f03fe9a5b8d4
tonan <tonantruong@hotmail.com>
2023-08-13 14:17:21 -0400

    completed ORMS to SQLAlchemy lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,34 @@
+from sqlalchemy import create_engine
 from models import Dog
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
+    
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog.name).filter(Dog.name == name).first()
+    
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog.name, Dog.breed).filter((Dog.name == name) | (Dog.breed == breed)).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+    return session
+
+
+
+
+
--- lib/models.py
+++ lib/models.py
@@ -11,3 +11,12 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    def __repr__(self):
+        return f'Dog: {self.id} ' \
+        + f'Name: {self.name}, ' \
+        + f'Breed: {self.breed}'
